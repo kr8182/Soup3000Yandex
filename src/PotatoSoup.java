80d88d5f13ce36f7a1cdf6af83033bb2b2adf2a9
kr8182 <kr81828@gmail.com>
2025-06-05 01:18:08 +0300

    5-q коммит

--- src/PotatoSoup.java
+++ src/PotatoSoup.java
@@ -2,27 +2,24 @@ public class PotatoSoup extends Bouillon {
     double potato;
     double carrot;
 
-    PotatoSoup(Bouillon bouillon, double meat, double potato, double carrot) {
-        super(bouillon, meat);
-        this.potato = potato;
-        this.carrot = carrot;
-    }
 
-    PotatoSoup(Bouillon bouillon, double potato, double carrot) {
-        super(bouillon, bouillon.meat);
-        this.potato = potato;
-        this.carrot = carrot;
-    }
-
-    PotatoSoup(double water, double salt, double meat, double potato, double carrot) {
+    public PotatoSoup(double water, double salt, double meat, double potato, double carrot) {
         super(water, salt, meat);
         this.potato = potato;
         this.carrot = carrot;
     }
 
+    public PotatoSoup(SoupBase soupBase, double meat, double potato, double carrot) {
+        super(soupBase, meat);
+        this.potato = potato;
+        this.carrot = carrot;
+    }
 
-
-    // реализуйте необходимые конструкторы
+    public PotatoSoup(Bouillon bouillon, double potato, double carrot) {
+        super(bouillon.water, bouillon.salt, bouillon.meat);
+        this.potato = potato;
+        this.carrot = carrot;
+    }
 
     @Override
     public void printIngredients() {
